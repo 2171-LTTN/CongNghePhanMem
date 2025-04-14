6ff2b547f9146eb0cb826e149f25f3a7a5430948
Phạm Thành Danh <danhnu3004@example.com>
2025-04-14 14:21:54 +0700

    thêm form Book_Management và sử lý code chức năng để cập nhật thông tin sách

---
+++ ProjectCNPM/ProjectCNPM/Views/Book_Management.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectCNPM.Views
+{
+    public partial class Book_Management : Form
+    {
+        public Book_Management()
+        {
+            InitializeComponent();
+        }
+
+        private void Book_Management_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
